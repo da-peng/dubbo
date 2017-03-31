5b2faab0c34776b25878625032f1e1ce5c8d501a
zongzhankui <zongzhankui@hotmail.com>
2017-03-31 21:38:22 +0800

    dubbo-spring-web-consumer

--- dubbo-spring-web-consumer/src/main/java/com/zkzong/dubbo/web/controller/ConsumerController.java
+++ dubbo-spring-web-consumer/src/main/java/com/zkzong/dubbo/web/controller/ConsumerController.java
@@ -1,18 +1,25 @@
 package com.zkzong.dubbo.web.controller;
 
+import com.zkzong.dubbo.web.facade.UserFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Zong on 2017/3/31.
  */
-@RestController
-@RequestMapping(value = "/consumer")
+@Controller
 public class ConsumerController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @Autowired
+    private UserFacade userFacade;
+
+    @RequestMapping(value = "/consumer", method = RequestMethod.GET)
+    @ResponseBody
     public String get() {
-        return "consumer";
+//        return "consumer";
+        return userFacade.queryUser().toString();
     }
 }
